ad619698006f7593d99cef41b11980b54c5c48b9
henryshop73 <henrymoranfreelance@gmail.com>
2022-07-19 14:26:39 -0500

    Agregando catch y finally a Promise

--- src/Promise/index.js
+++ src/Promise/index.js
@@ -14,4 +14,6 @@ const countCows = new Promise(function(resolve, reject) {
 
 countCows.then((result) => {
     console.log(result);
-})
+}).catch((error) => {
+    console.log(error);
+}).finally(() => console.log('Finally'));
